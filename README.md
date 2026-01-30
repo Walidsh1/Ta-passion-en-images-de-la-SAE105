aa9c7b584771ec9b2e6fe4f428e8668fe1c00225
Walidsh1 <walidelshennawy32@gmail.com>
2026-01-30 16:57:06 +0100

    :tada: site SAE105

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Ta-passion-en-images-de-la-SAE105
+# À la découverte des singes
+
+## URL du site
+- URL en ligne : http://elshennawy.projetsmmichamps.fr/sae105/
+
+## Installation locale avec XAMPP
+1. Copier le dossier du site dans le dossier `htdocs` de XAMPP.
+2. Démarrer le module **Apache** dans le panneau de contrôle XAMPP.
+3. Dans le navigateur, aller sur : `http://localhost/[nom_du_dossier]/`.
+
+## Fonctionnalités principales
+- Affichage d’une galerie de singes à partir d’un fichier JSON.
+- Popup permettant d’agrandir les images.
+- Formulaire pour créer une fiche de singe avec aperçu en temps réel.
+- Envoi des informations du formulaire vers une API avec un message contenant le titre, la description, le choix et l’URL de l’image.
+
+## Référence Opquast
+- Checklist / ressource utilisée : [lien vers la page Opquast ou ton résultat].
---
+++ data.js
@@ -0,0 +1,56 @@
+// let images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+// let titles = ["L'Atèle", "Le Macaque à longue queue", "Le Gorille", "L'Orang-outan"];
+// let descriptionsImages = [
+//  "Description de l'Atèle...vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv",
+//   "Description du Macaque à longue queue...vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv",
+//   "Description du Gorille...vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv",
+//   "Description de l'Orang-outan...cvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+// ];
+
+// let data = [
+//   {
+//     image: "image1.jpg",
+//     titre: "L'Atèle",
+//     description: "Ta description de l'Atèle...",
+//     choix: "choix1",
+//     urlImage: "https://cdn.pixabay.com/photo/2024/09/08/16/37/spider-monkey-9032764_1280.jpg",
+//     urlSource : "https://pixabay.com/fr/photos/spider-monkey-singe-nature-primate-9032764/",
+//     nomSite : "Pixabay",
+//     urlAuteur : "https://pixabay.com/fr/users/dominikrh-12119308/",
+//     nomAuteur : "DominikRh"
+//   },
+//   {
+//     image: "image2.jpg",
+//     titre: "Le Macaque à longue queue",
+//     description: "Ta description du macaque...",
+//     choix: "choix2",
+//     urlImage: "https://images.pexels.com/photos/34897098/pexels-photo-34897098.jpeg",
+//     urlSource : "https://www.pexels.com/fr-fr/photo/34897098/",
+//     nomSite : "Pexels",
+//     urlAuteur : "https://www.pexels.com/fr-fr/@farlight/",
+//     nomAuteur : "Lars H Knudsen"
+
+//   },
+//   {
+//     image: "image3.jpg",
+//     titre: "Le Gorille",
+//     description: "Ta description du gorille...",
+//     choix: "choix3",
+//     urlImage: "https://images.pexels.com/photos/1238277/pexels-photo-1238277.jpeg",
+//     urlSource : "https://www.pexels.com/fr-fr/photo/gorille-autour-des-buissons-1238277/",
+//     nomSite : "Pexels",
+//     urlAuteur : "https://www.pexels.com/fr-fr/@francesco-ungaro/",
+//     nomAuteur : "Francesco Ungaro"
+//   },
+//   {
+//     image: "image4.jpg",
+//     titre: "L'Orang-outan",
+//     description: "Ta description de l'orang-outan...",
+//     choix: "choix4",
+//     urlImage: "https://images.pexels.com/photos/26926253/pexels-photo-26926253.jpeg",
+//     urlSource : "https://www.pexels.com/fr-fr/photo/un-regard-triste-26926253/",
+//     nomSite : "Pexels",
+//     urlAuteur : "https://www.pexels.com/fr-fr/@florian-kriechbaumer-1479976889/",
+//     nomAuteur : "Florian Kriechbaumer"
+//   }
+// ];
